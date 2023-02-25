f11ee7b07f441c158d6037a254b53bb3f38fb1ee
marian <elias-marian@web.de>
2023-02-25 22:42:56 +0100

    :bento: Added custom data loader

---
+++ src/main/kotlin/loaders/CustomDataLoader.kt
@@ -0,0 +1,20 @@
+package loaders
+
+class CustomDataLoader {
+
+    data class Data(val x: Double, val y: Double, val classification: Byte)
+
+    fun loadSamples(): List<Data> {
+        val text = this::class.java.getResource("/custom-data/data.sample")?.readText() ?: error("File not found")
+        return text.split("\n".toRegex()).mapNotNull { line ->
+            if (line.startsWith("#")) return@mapNotNull null
+            val data = line.split("|")
+            Data(
+                x = data[0].toDoubleOrNull() ?: 0.0,
+                y = data[1].toDoubleOrNull() ?: 0.0,
+                classification = data[2].toByteOrNull() ?: 0
+            )
+        }
+    }
+
+}
